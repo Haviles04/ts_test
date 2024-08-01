ed8017d06b8849887909d05cc36f816b67d5fed7
Henry <haviles04@gmail.com>
2024-08-01 17:58:35 -0400

    type: updated request and response types

--- api/controllers/add-blog.ts
+++ api/controllers/add-blog.ts
@@ -1,8 +1,9 @@
 import { BlogType } from '../types/blog';
+import e from 'express';
 
 declare var Blog: any;
 
-module.exports = async function (req: any, res: any) {
+module.exports = async function (req: e.Request<{}, {}, BlogType>, res: e.Response) {
     try {
         const {title, author, pages} = req.body;
         const newBlog: BlogType = await Blog.create({
@@ -13,12 +14,12 @@ module.exports = async function (req: any, res: any) {
     
         const blogTitle: string = newBlog.title;
         
-        return res.status(200).json({ blog: newBlog });
+        return res.status(200).json({ blog: newBlog, title: blogTitle });
 
 
 
     }
     catch(err) {
-        return res.badRequest(err);
+        return res.status(500).json(err);
     }
 }
